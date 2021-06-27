e7b81c17a6bab54f875e9a5e078b08e71dbc0f04
1-8192 <allegranzia@gmail.com>
2021-06-27 17:30:53 -0400

    updaating some packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: `Alessandro Allegranzi`,
-    description: `I am a Boston based full stack web developer`,
-    author: `Alessandro Allegranzi`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `markdown-pages`,
         path: `${__dirname}/src/markdown-pages`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -23,14 +30,15 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#5495b8`,
-        theme_color: `#5495b8`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-transformer-remark`,
   ],
 }
